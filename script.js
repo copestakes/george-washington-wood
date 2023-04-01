81d8d670c09f86502d2bf50cfe2e4981afa6297a
copestakes <davecopestakes@gmail.com>
2023-04-01 01:10:54 -0400

    updates

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function checkAll() {
+  const input = document.querySelector('#check');
+  input.checked = false;
+}
+window.onload = function () {
+  window.addEventListener('resize', checkAll, false);
+};
